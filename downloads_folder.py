6ba3877b674d94b769c243f722bec1c5d2d5c74c
Askaiant <44230751+Askaiant@users.noreply.github.com>
2020-01-07 22:25:50 +0100

    Update downloads_folder.py

--- downloads_folder.py
+++ downloads_folder.py
@@ -4,7 +4,7 @@ import shutil
 # Write the name of the directory here, that needs to get sorted
 path = 'enter your path here'
 
-# This will create a list with all the filename that is there in the directory
+# List with all the filename that are in the directory
 list_ = os.listdir(path)
 
 # This will go through each and every file
@@ -15,14 +15,14 @@ for file_ in list_:
     else:
         continue
 
-    # This is going to store the extension type
+    # Extension type
     ext = ext[1:]
 
-    # This will move the file to the directory where the name 'ext' already exists
+    # File to the directory of the name 'ext' if already exists
     if os.path.exists(path + '/' + ext):
         shutil.move(path + '/' + file_, path + '/' + ext + '/' + file_)
 
-    # This will create a new directory, if the directory does not already exist
+    # Creates a new directory, if the directory does not already exist
     else:
         os.makedirs(path + '/' + ext)
         shutil.move(path + '/' + file_, path + '/' + ext + '/' + file_)
