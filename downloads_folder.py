a9c608aa97db5af3f5073cfdb66126bf9e6f12fc
Askaiant <44230751+Askaiant@users.noreply.github.com>
2020-01-07 21:19:33 +0100

    Add files via upload

---
+++ downloads_folder.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+# Write the name of the directory here, that needs to get sorted
+path = 'E:/Downloads'
+
+# This will create a list with all the filename that is there in the directory
+list_ = os.listdir(path)
+
+# This will go through each and every file
+for file_ in list_:
+    # This checks if 'file_' is indeed a file
+    if os.path.isfile(path + '/' + file_):
+        name, ext = os.path.splitext(file_)
+    else:
+        continue
+
+    # This is going to store the extension type
+    ext = ext[1:]
+
+    # This will move the file to the directory where the name 'ext' already exists
+    if os.path.exists(path + '/' + ext):
+        shutil.move(path + '/' + file_, path + '/' + ext + '/' + file_)
+
+    # This will create a new directory, if the directory does not already exist
+    else:
+        os.makedirs(path + '/' + ext)
+        shutil.move(path + '/' + file_, path + '/' + ext + '/' + file_)
